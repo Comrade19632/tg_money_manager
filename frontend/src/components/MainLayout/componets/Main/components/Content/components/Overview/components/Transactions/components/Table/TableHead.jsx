65e2010266c8f12a47dd41a884d0c88ebd11539f
Artem Kasheev <comrade19632@gmail.com>
2022-06-16 21:58:30 +0300

    quick frontend fixed

--- frontend/src/components/MainLayout/componets/Main/components/Content/components/Overview/components/Transactions/components/Table/TableHead.jsx
+++ frontend/src/components/MainLayout/componets/Main/components/Content/components/Overview/components/Transactions/components/Table/TableHead.jsx
@@ -4,7 +4,7 @@ import style from './index.module.sass'
 
 const TableHead = () => (
   <div className={classNames(style.flexTable, style.header)} role="rowgroup">
-    <div className={classNames(style.flexRow, style.amountCol)} role="columnheader">Сумма,</div>
+    <div className={classNames(style.flexRow, style.amountCol)} role="columnheader">Сумма, р.</div>
     <div className={classNames(style.flexRow, style.typeCol)} role="columnheader">Тип</div>
     <div className={classNames(style.flexRow, style.categoryCol)} role="columnheader">Категория</div>
     <div className={classNames(style.flexRow, style.dateCol)} role="columnheader">Дата</div>
