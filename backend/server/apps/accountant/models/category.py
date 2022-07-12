c9805220a42246cd3171ce1762d27f8f97fe673f
Artem Kasheev <comrade19632@gmail.com>
2022-07-12 15:02:39 +0300

    unique_title categorires

--- backend/server/apps/accountant/models/category.py
+++ backend/server/apps/accountant/models/category.py
@@ -1,4 +1,4 @@
-from django.db.models import CASCADE, CharField, ForeignKey, IntegerField
+from django.db.models import CASCADE, CharField, ForeignKey, IntegerField, UniqueConstraint
 
 from common.models import ActiveModel, TimeStampedModel
 
@@ -9,3 +9,8 @@ class Category(ActiveModel, TimeStampedModel):
     title = CharField(max_length=128, default="Без названия")
     user = ForeignKey("users.User", on_delete=CASCADE)
     enum_type = IntegerField(choices=EnumType.choices, default=EnumType.OUTCOME)
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=["title", "user", "enum_type"], name="unique_title")
+        ]
